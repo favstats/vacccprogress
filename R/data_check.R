74e30fed60c6e05c818ab8c4de34a1e55c4488b7
Fabio Votta <fabio.votta@gmail.com>
2021-05-19 13:15:03 +0200

    fix

--- R/data_check.R
+++ R/data_check.R
@@ -1,24 +1,21 @@
 
-library(here)
-
 vaccs <- read.csv("https://raw.githubusercontent.com/owid/covid-19-data/master/public/data/vaccinations/vaccinations.csv")
-
 
 vacc_date <- max(as.Date(unique(vaccs[vaccs$location == "World",]$date)), na.rm = T)
 
-last_date <- readLines(here::here("tmp", "lastdate.txt"))
+last_date <- readLines("lastdate.txt")
 
 
 # check whether the new data has already been tweeted
 if (vacc_date != last_date) {
     # no new data
     print("No new data. Skipping update.")
-    cat("no_update", file = here::here("tmp", "update.txt"))
+    cat("no_update", file = "/tmp/update.txt")
     
     quit(status = 0, save = "no")
 }
 
 
 # write out lastdate and update for gh actions
-cat(last_date, file = here::here("tmp", "lastdate.txt"))
-cat("update", file = here::here("tmp", "update.txt"))
+cat(last_date, file = "lastdate.txt")
+cat("update", file = "/tmp/update.txt")
